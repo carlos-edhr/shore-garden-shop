3762a3fd3fe6706ff4a1e7993395e1a3dfc70ee1
carlos-edhr <carlos.edhr@protonmail.com>
2021-04-07 03:17:16 -0700

    Products details views and url added

--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
+from .models import Product 
+
+
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World!</h1>")
+
+
+def product_detail_view(request, id):
+    try:
+        obj = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http4
+    return HttpResponse(f"Product id {obj.id}")
+
+def product_api_detail_view(request):
+    obj = Product.objects.get(id=id)
+    try:
+        obj = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        return JsonResponse({"message": "Not Found"},
+        status_code=404)
+    return JsonResponse({"id": obj.id})
